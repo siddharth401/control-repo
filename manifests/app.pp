f3fcba31f7773a8cdd2f692df28e116e76ee3046
root <root@puppet.codespaces.io>
2019-05-05 08:25:53 +0000

    manifests added

---
+++ manifests/app.pp
@@ -0,0 +1,51 @@
+node 'node1' {
+
+notify{"*****NODE1 Message******":}
+include base
+
+class {'::tomcat':
+
+ user   => 'tomcat',
+ group  => 'tomcat',
+ service_state  => 'running',
+ 
+}
+}
+
+node default { 
+   
+   notify{'checkpoint _1' :
+   
+   
+  message       => 'Applying default BLOCK
+  
+                    Look like You are using default block
+  
+  
+  
+  
+  '
+}
+}
+
+node 'node2' { 
+
+
+notify{ "*****NODE2 BLOCK******" :}
+
+class{'::tomcat':
+
+user    => 'tomcat',
+group   => 'root',
+service_state  => 'running',
+catalina_pid    => '/tmp/tomcat.pid',
+
+}
+
+include java::install
+include base
+include tomcat::install
+include tomcat::config
+include tomcat::service
+
+}
